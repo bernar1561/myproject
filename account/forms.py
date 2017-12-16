d770862b8927b760f7bbd3a3a269f4651dc1045b
admin <admin@iMac-admin.local>
2017-12-16 14:16:56 +0600

    myproject 1.5

--- account/forms.py
+++ account/forms.py
@@ -12,7 +12,7 @@ class UserEditForm(forms.ModelForm):
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('date_of_birth', 'photo')
+        fields = ('date_of_birth', 'avatar')
 
 
 class UserRegistrationForm(forms.ModelForm):
--- account/models.py
+++ account/models.py
@@ -3,10 +3,27 @@ from django.conf import settings
 
 
 class Profile(models.Model):
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+    avatar = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+    vk_link = models.URLField(verbose_name='ссылка на Vk')
+    fb_link = models.URLField(verbose_name='ссылка на facebook')
+    twitter_link = models.URLField(verbose_name='ссылка на twitter')
+    inst_link = models.URLField(verbose_name='ссылка на instagram')
 
     def __str__(self):
-        return 'Profile for user {}'.format(self.user.username)
+        return 'Профиль для пользователя {}'.format(self.user.username)
 
+
+class Photo(models.Model):
+    description = models.CharField(verbose_name='Описание', max_length=300, default=None, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+    photo_like = models.IntegerField(verbose_name='Количество лайков', default=0, blank=True)
+
+    def __str__(self):
+        return 'Фотки пользователя %s %s' % (self.description, self.user.username)
